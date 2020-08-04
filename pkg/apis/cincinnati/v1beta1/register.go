bbcaeaafc5074524cfec1275f2c6592423498429
W. Trevor King <wking@tremily.us>
2020-08-04 11:19:01 -0700

    pkg/apis/cincinnati/v1beta1: Drop redundant doc.go
    We've had a package docstring in both doc.go and register.go since
    v1alpha1 in 52a976598c (first code commit, 2020-04-03, #1).  But
    there's no reason to have a doc.go declaration when register.go
    already defines the same material.
    
    Also add a trailing period to the docstring sentence and capitalize
    the Cincinnati proper noun.  And remove the boilerplate comment,
    because this is a hand-maintained file, even if changes will be rare.

--- pkg/apis/cincinnati/v1beta1/register.go
+++ pkg/apis/cincinnati/v1beta1/register.go
@@ -1,6 +1,4 @@
-// NOTE: Boilerplate only.  Ignore this file.
-
-// Package v1beta1 contains API Schema definitions for the cincinnati v1beta1 API group
+// Package v1beta1 contains API Schema definitions for the Cincinnati v1beta1 API group.
 // +k8s:deepcopy-gen=package,register
 // +groupName=cincinnati.openshift.io
 package v1beta1
