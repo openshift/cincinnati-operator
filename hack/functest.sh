bf9db826e4cd86283251c09f819eceda2376990b
W. Trevor King <wking@tremily.us>
2020-10-19 12:08:13 -0700

    functests: Drop Ginkgo dependency
    Use Go's built-in testing package instead.  This avoids issues like [1]:
    
      Retrieving ginkgo and gomega build dependencies
      go: github.com/openshift/cluster-image-registry-operator@v0.0.0-20200415091009-99c06ee64540 requires
      		github.com/openshift/library-go@v0.0.0-20200327125526-163b2f0d6264 requires
      		bitbucket.org/ww/goautoneg@v0.0.0-20120707110453-75cd24fc2f2c: reading https://api.bitbucket.org/2.0/repositories/ww/goautoneg?fields=scm: 404 Not Found
      go: github.com/openshift/cluster-image-registry-operator@v0.0.0-20200415091009-99c06ee64540 requires
      		github.com/openshift/library-go@v0.0.0-20200327125526-163b2f0d6264 requires
      		bitbucket.org/ww/goautoneg@v0.0.0-20120707110453-75cd24fc2f2c: reading https://api.bitbucket.org/2.0/repositories/ww/goautoneg?fields=scm: 404 Not Found
      hack/functest.sh: line 17: /go/bin/ginkgo: No such file or directory
      hack/functest.sh: line 19: /go/bin/ginkgo: No such file or directory
    
    The tests are simple enough that Ginkgo structuring was overkill
    anyway.  Generated by manually editing files in functests, and then
    running:
    
      $ go mod tidy
      $ go mod vendor
      $ git add -A go.* vendor
    
    using:
    
      $ go version
      go version go1.15.2 linux/amd64
    
    [1]: https://github.com/openshift/cincinnati-operator/pull/66#issuecomment-703931608

--- hack/functest.sh
+++ hack/functest.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-GOBIN="${GOBIN:-$GOPATH/bin}"
-GINKGO=$GOBIN/ginkgo
-
-if ! [ -x "$GINKGO" ]; then
-	# The golang-1.13 image used in OpenShift CI enforces vendoring and go get is disabled.
-	# Workaround that by unsetting GOFLAGS.
-	export GOFLAGS=""
-	echo "Retrieving ginkgo and gomega build dependencies"
-	go get github.com/onsi/ginkgo/ginkgo
-	go get github.com/onsi/gomega/...
-else
-	echo "GINKO binary found at $GINKGO"
-fi
-
-"$GOBIN"/ginkgo build functests/
-# Run functional tests
-"$GOBIN"/ginkgo -v --slowSpecThreshold=500 --randomizeAllSpecs  --progress functests
+go test ./functests/...
