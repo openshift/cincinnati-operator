d1d4f76f3d54c34c11597f61d531264f19022c2b
Lalatendu Mohanty <lmohanty@redhat.com>
2021-03-29 16:25:58 -0400

    Changing the name to make OSBS auto repo/registry replacements to work
    Refer
    https://osbs.readthedocs.io/en/latest/users.html#pullspec-locations for
    details
    
    Also changed OPERATOR_IMAGE to RELATED_IMAGE_OPERATOR for consistency
    
    Signed-off-by: Lalatendu Mohanty <lmohanty@redhat.com>

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -7,14 +7,14 @@ set -e
 DEFAULT_OPERATOR_IMAGE="controller:latest"
 DEFAULT_OPERAND_IMAGE="quay.io/app-sre/cincinnati:2873c6b"
 
-OPERATOR_IMAGE="${OPERATOR_IMAGE:-${DEFAULT_OPERATOR_IMAGE}}"
-OPERAND_IMAGE="${OPERAND_IMAGE:-${DEFAULT_OPERAND_IMAGE}}"
+RELATED_OPERATOR_IMAGE="${RELATED_IMAGE_OPERATOR:-${DEFAULT_OPERATOR_IMAGE}}"
+RELATED_OPERAND_IMAGE="${RELATED_IMAGE_OPERAND:-${DEFAULT_OPERAND_IMAGE}}"
 
 if [ -n "$OPENSHIFT_BUILD_NAMESPACE" ]; then
-	OPERATOR_IMAGE="registry.svc.ci.openshift.org/${OPENSHIFT_BUILD_NAMESPACE}/stable:updateservice-operator"
+	RELATED_OPERATOR_IMAGE="registry.svc.ci.openshift.org/${OPENSHIFT_BUILD_NAMESPACE}/stable:updateservice-operator"
 	GRAPH_DATA_IMAGE="registry.svc.ci.openshift.org/${OPENSHIFT_BUILD_NAMESPACE}/stable:updateservice-graph-data-container"
 
-	echo "Openshift CI detected, deploying using image $OPERATOR_IMAGE and ${GRAPH_DATA_IMAGE}"
+	echo "Openshift CI detected, deploying using image $RELATED_OPERATOR_IMAGE and ${GRAPH_DATA_IMAGE}"
 
 else
 	if ! [ -n "$KUBECONFIG" ]; then
@@ -27,8 +27,8 @@ else
 	fi
 fi
 
-sed -i "s|quay.io/cincinnati/cincinnati:latest|$OPERAND_IMAGE|" config/manager/manager.yaml
-sed -i "s|$DEFAULT_OPERATOR_IMAGE|$OPERATOR_IMAGE|" config/manager/manager.yaml
+sed -i "s|quay.io/cincinnati/cincinnati:latest|$RELATED_OPERAND_IMAGE|" config/manager/manager.yaml
+sed -i "s|$DEFAULT_OPERATOR_IMAGE|$RELATED_OPERATOR_IMAGE|" config/manager/manager.yaml
 sed -i "s|your-registry/your-repo/your-init-container|$GRAPH_DATA_IMAGE|" config/samples/updateservice.operator.openshift.io_v1_updateservice_cr.yaml
 
 NAMESPACE="openshift-updateservice"
